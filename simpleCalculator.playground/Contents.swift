c90ac25fe11bc42b9eab7f4b776a968e99b7951f
EllafCS <elafal.jaramez2018@gmail.com>
2024-08-16 22:40:42 +0300

    Initial Commit

---
+++ simpleCalculator.playground/Contents.swift
@@ -0,0 +1,30 @@
+
+// Function for Additon
+func calculateAddition (num1 : Int , num2 : Int) -> Int{
+    return num1 + num2
+}
+let totalAddition = calculateAddition(num1: 25, num2: 25)
+print ("The Addition Total 25 + 25 = \(totalAddition)")
+
+// Function for Subtraction
+func calculateSubtraction (num1 : Int , num2 : Int) -> Int{
+    return num1 - num2
+}
+let totalSubtraction = calculateSubtraction(num1: 25, num2: 25)
+print ("The Subtraction Total 25 - 25 = \(totalSubtraction)")
+
+// Function for Multipliction
+func calculateMultipliction (num1 : Int , num2 : Int) -> Int{
+    return num1 * num2
+}
+let totalMultipliction = calculateMultipliction(num1: 25, num2: 25)
+print ("The Multipliction Total 25 * 25 = \(totalMultipliction)")
+
+
+// Function for Divison
+func calculateDivison (num1 : Int , num2 : Int) -> Int{
+    return num1 / num2
+}
+let totalDivison = calculateDivison(num1: 25, num2: 25)
+print ("The Divison Total 25 / 25 = \(totalDivison)")
+
